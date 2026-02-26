0ff395bd38fbebb3ad068ae0525fa0d113b5f033
Lakmal Wijerathna <samdynmic@gmail.com>
2026-02-26 11:15:49 +0100

    nabvar

--- resources/views/inc/navbar_detailed.blade.php
+++ resources/views/inc/navbar_detailed.blade.php
@@ -6,8 +6,9 @@
     
       <header class="lh-1 py-3 d-none d-lg-block" id="header">
                <div class="container">
+               
                  <div class="row  justify-content-between align-items-center">
-                    <div class="col-4 pt-1">
+                    <div class="col-4">
                         <div class="navbar-links">
                            
                         <a class="link-secondary non-hover" href="#">{{__('Home')}}</a>
@@ -21,7 +22,7 @@
                         </a>
                     </div>
                     <div class="col-4 d-flex justify-content-end align-items-center">
-                        <div class="navbar-links">
+                        <div class="navbar-links d-flex">
                             
                             <a  href="#" class="link-secondary non-hover">
                                 <i class="bi bi-person"></i>
@@ -41,7 +42,7 @@
                 <div class="container">
                     <div class="row flex-nowrap justify-content-between align-items-center">
                     <div class="col-4 pt-1">
-                        <div class="navbar-links">
+                        <div class="navbar-links d-flex">
                             {{-- <a class="link-secondary" href="#">{{__('Home')}}</a>
                         <a class="link-secondary" href="#">{{__('Products')}}</a> --}}
                         <a class="link-secondary" href="#"><i class="bi bi-list" id="menu_icon"></i></a>
@@ -55,7 +56,81 @@
                         </a>
                     </div>
                     <div class="col-4 d-flex justify-content-end align-items-center">
+                        <div class="navbar-links d-flex">
+                            <a class="link-secondary" href="#"><i class="bi bi-person"></i></a>
+                            <div>
+                               @livewire('shop.cartbutton')
+
+                            </div>
+                           
+                            {{-- <a class="link-secondary" href="#">{{__('Cart')}}</a> --}}
+                        </div>
+                        
+                    </div>
+                </div>
+                </div>
+            </header>
+    @include('inc.sidenav')
+    {{--  --}}
+</nav>
+@else
+{{--  --}}
+<nav class="position-0 fixed-top bg-white">
+    {{-- <div class="prm-bar p-1 text-center"><small class="fw-bold">{{ __('We ship 1-3 days in Croatia for free over 100 ')}}</small></div> --}}
+    
+      <header class="lh-1 py-3 d-none d-lg-block">
+               <div class="container">
+               
+                 <div class="row  justify-content-between align-items-center">
+                    <div class="col-4">
                         <div class="navbar-links">
+                           
+                        <a class="link-secondary non-hover scrolled" href="#">{{__('Home')}}</a>
+                        <a class="link-secondary non-hover scrolled" href="#">{{__('Tallow Skincare')}}</a>
+                        <a class="link-secondary non-hover scrolled" href="#">{{__('contact')}}</a>
+                        </div>
+                    </div>
+                    <div class="col-4 text-center">
+                        <a class="blog-header-logo text-body-emphasis text-decoration-none" href="#">
+                            <img src="{{asset('media/images/logo.png')}}" alt="" class="logo-md">
+                        </a>
+                    </div>
+                    <div class="col-4 d-flex justify-content-end align-items-center">
+                        <div class="navbar-links d-flex">
+                            
+                            <a  href="#" class="link-secondary non-hover scrolled">
+                                <i class="bi bi-person"></i>
+                            </a>
+                             <div>
+                               @livewire('shop.cartbutton')
+
+                            </div>
+                            {{-- <a class="link-secondary" href="#">{{__('Cart')}}</a> --}}
+                        </div>
+                        
+                    </div>
+                </div>
+               </div>
+            </header>
+            <header class="d-block d-lg-none bg-white">
+                <div class="container">
+                    <div class="row flex-nowrap justify-content-between align-items-center">
+                    <div class="col-4 pt-1">
+                        <div class="navbar-links d-flex">
+                            {{-- <a class="link-secondary" href="#">{{__('Home')}}</a>
+                        <a class="link-secondary" href="#">{{__('Products')}}</a> --}}
+                        <a class="link-secondary scrolled" href="#"><i class="bi bi-list" id="menu_icon"></i></a>
+                        <a class="link-secondary scrolled" href="#"><i class="bi bi-search"></i></a>
+                        
+                        </div>
+                    </div>
+                    <div class="col-4 text-center">
+                        <a class="blog-header-logo text-body-emphasis text-decoration-none" href="#">
+                            <img src="{{asset('media/images/logo.png')}}" alt="" class="logo-md">
+                        </a>
+                    </div>
+                    <div class="col-4 d-flex justify-content-end align-items-center">
+                        <div class="navbar-links d-flex">
                             <a class="link-secondary" href="#"><i class="bi bi-person"></i></a>
                             <div>
                                @livewire('shop.cartbutton')
