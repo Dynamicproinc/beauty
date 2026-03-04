ef095cfe35b777b91fad35181b945d9d1a6c4b1f
Lakmal Wijerathna <samdynmic@gmail.com>
2026-03-04 11:15:46 +0100

    sdsD

--- resources/views/livewire/shop/esubscription.blade.php
+++ resources/views/livewire/shop/esubscription.blade.php
@@ -1,39 +1,41 @@
 <div>
     <div class="container">
         <form wire:submit="subscribe">
-        <h5 class="">{{ __('Subscribe to our newsletter') }}</h5>
-        <p class="">
-            {{ __('Subscribe to our newsletter and be the first to receive the latest updates, news, tips, and special offers directly in your inbox. Join our community today and never miss important updates.') }}
-        </p>
+            <h5 class="">{{ __('Subscribe to our newsletter') }}</h5>
+            <p class="">
+                {{ __('Subscribe to our newsletter and be the first to receive the latest updates, news, tips, and special offers directly in your inbox. Join our community today and never miss important updates.') }}
+            </p>
 
-        {{-- <h6 class="fw-bold">{{ __('Subscribe to our newsletter') }}</h6> --}}
-        <div class="">
-            <label for="newsletterEmail" class="visually-hidden">
-                {{ _('Email address') }}
-            </label>
-            <input id="newsletterEmail" type="email" class="form-control mb-3" placeholder="{{ _('Email address') }}"
-                required wire:model="email">
-            <div class="mb-3">
-                @error('email')
-                    <small class="text-danger">{{ $message }}</small>
-                @enderror
+            {{-- <h6 class="fw-bold">{{ __('Subscribe to our newsletter') }}</h6> --}}
+            <div class="">
+                <label for="newsletterEmail" class="visually-hidden">
+                    {{ _('Email address') }}
+                </label>
+                <input id="newsletterEmail" type="email" class="form-control mb-3" placeholder="{{ _('Email address') }}"
+                    required wire:model="email">
+                <div class="mb-3">
+                    @error('email')
+                        <small class="text-danger">{{ $message }}</small>
+                    @enderror
+                </div>
+                <div class="mb-3" wire:ignore>
+                    <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}"
+                        data-callback="onRecaptchaSuccess">
+                    </div>
+
+                    @error('g-recaptcha-response')
+                        <div class="text-danger mt-2">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+
+
+                <button class="tt_btn_theme w-100 rounded-0">
+                    {{ __('Subscribe') }}
+                </button>
             </div>
-            <div class="mb-3">
-                                <div class="g-recaptcha" data-sitekey="{{ env('RECAPTCHA_SITE_KEY') }}"></div>
-                                @error('g-recaptcha-response')
-                                    <div class="text-danger mt-2">
-                                        {{ $message }}
-                                    </div>
-                                @enderror
+        </form>
 
-                            </div>
-           
-
-            <button class="tt_btn_theme w-100 rounded-0">
-                {{ __('Subscribe') }}
-            </button>
-        </div>
-    </form>
-    
     </div>
 </div>
