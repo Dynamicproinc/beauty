8e8412e29091a0c52bb22f8102cf1eb4bd321826
Lakmal Wijerathna <samdynmic@gmail.com>
2026-03-23 11:35:07 +0100

    our story

--- resources/views/tallow_theme.blade.php
+++ resources/views/tallow_theme.blade.php
@@ -464,10 +464,15 @@
                                     <h1 class="tt-text-hero-md text-uppercase">
                                         {{ __('a word from our founder') }}
                                     </h1>
-                                    <p>
+                                    {{-- <p>
                                         {{ __('At Tallow Skincare, we believe that skincare should be simple, honest, and close to nature. Our products are carefully crafted using high-quality, natural tallow to nourish and protect your skin the way it was meant to be cared for. Inspired by traditional wisdom and supported by a commitment to purity, we focus on clean ingredients without unnecessary additives. Every jar represents our dedication to quality, sustainability, and your skin’s natural balance. Thank you for trusting us to be part of your daily self-care journey. We are proud to create products that bring nature back to skincare. 🌿') }}
 
-                                    </p>
+                                    </p> --}}
+                                     <p>{{__('After a year and a half of research, testing, and sharing experiences, we founded Tallow skin care in 2025.')}}</p>
+                                    <p>{{__('The base for the products, beef tallow, comes from the brand The M Brothers, who breed Black Angus, which is a prerequisite for us to know what we are working with and to have a reliable base for tallow cosmetics.')}}</p>
+                                    <p>{{__('To begin with, we have formulated unique, multi-purpose products that meet the skins needs for hydration and nutrition and complete them with natural active ingredients.')}}</p>
+                                    <p>{{__('We believe that skin health is not only determined by the product we apply to the skin, but is much more complex, and therefore we want to raise awareness of the importance of the health of our entire body.')}}</p>
+                                    <p>{{__('Let your skin be a mirror of whats going on inside!')}}</p>
                                     <div>
                                         <h6>
                                             Lorena Maturanec
