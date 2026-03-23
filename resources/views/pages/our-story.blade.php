490271e0aba8d6462260a2ab554641f2cf992c4a
Lakmal Wijerathna <samdynmic@gmail.com>
2026-03-23 11:46:53 +0100

    our story

--- resources/views/pages/our-story.blade.php
+++ resources/views/pages/our-story.blade.php
@@ -16,8 +16,8 @@
                     <p class="tt-text-hero-md text-uppercase text-center mb-0">
                                     {{ __('Natural balance is the most important.') }}
                                 </p>
-                                <div class="mt-3 text-center">
-                                    <p class="">{{__('Tallow skin care was born out of personal enthusiasm and over the past year and a half has grown into a real project to create natural skin care products.')}}</p>
+                                <div class="mt-3 text-center p-3">
+                                    <p class="p-3">{{__('Tallow skin care was born out of personal enthusiasm and over the past year and a half has grown into a real project to create natural skin care products.')}}</p>
                                     {{-- <p>{{__('After a year and a half of research, testing, and sharing experiences, we founded Tallow skin care in 2025.')}}</p>
                                     <p>{{__('The base for the products, beef tallow, comes from the brand The M Brothers, who breed Black Angus, which is a prerequisite for us to know what we are working with and to have a reliable base for tallow cosmetics.')}}</p>
                                     <p>{{__('To begin with, we have formulated unique, multi-purpose products that meet the skins needs for hydration and nutrition and complete them with natural active ingredients.')}}</p>
@@ -31,11 +31,10 @@
         </div>
         </section>
          <section class="br-1-bg medium-section mb-0" id="founders-word">
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="row">
-                        <div class="col-lg-6 mb-3">
-                            <div class="d-flex align-items-center justify-content-center mid-item">
+          <div class="row justify-content-center">
+            <div class="col-lg-6">
+                  <div class="container">
+                  <div class="">
                                 <div>
                                     <h1 class="tt-text-hero-md text-uppercase">
                                         {{ __('a word from our founder') }}
@@ -57,16 +56,23 @@
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-lg-6">
+                             <div class="text-center">
                             <div class="f-img-wr">
                                 <img src="https://www.tallows.de/cdn/shop/files/ece-tallow.webp?v=1758472868&width=1000"
                                     alt="tallow-skin-care" class="">
                             </div>
                         </div>
+                <div class="row justify-content-center">
+                    <div class="row">
+                        <div class="col-lg-6 mb-3">
+                          
+                        </div>
+                       
                     </div>
                 </div>
             </div>
+            </div>
+          </div>
         </section>
     </div>
 @endsection
