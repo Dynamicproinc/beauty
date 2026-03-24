12914f3f4c1c56bc9a0f70d5a3e60ec381f38526
Lakmal Wijerathna <samdynmic@gmail.com>
2026-03-24 11:27:42 +0100

    dsdsd

--- resources/views/inc/cookie.blade.php
+++ resources/views/inc/cookie.blade.php
@@ -2,17 +2,16 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-lg-8 mb-3 mb-lg-0">
-                <h5 class="text-white">We use cookies</h5>
+                <h5 class="text-white">{{__('We use cookies')}}</h5>
                 <p class="mb-0 small">
-                    We use necessary cookies to make our website work. With your consent, we may also use
-                    analytics and marketing cookies to improve your experience.
+                    {{__('We use necessary cookies to make our website work. With your consent, we may also use analytics and marketing cookies to improve your experience.')}}
                 </p>
             </div>
             <div class="col-lg-4 text-lg-end">
-                <button class="btn btn-light btn-sm me-2" onclick="acceptAllCookies()">Accept All</button>
-                <button class="btn btn-outline-light btn-sm me-2" onclick="rejectCookies()">Reject</button>
+                <button class="btn btn-light btn-sm me-2" onclick="acceptAllCookies()">{{__('Accept All')}}</button>
+                <button class="btn btn-outline-light btn-sm me-2" onclick="rejectCookies()">{{__('Reject')}}</button>
                 <button class="btn btn-secondary btn-sm" data-bs-toggle="modal" data-bs-target="#cookieModal">
-                    Settings
+                    {{__('Settings')}}
                 </button>
             </div>
         </div>
@@ -24,33 +23,33 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Cookie Preferences</h5>
+                <h5 class="modal-title">{{__('Cookie Preferences')}}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
                 <div class="form-check form-switch mb-3">
                     <input class="form-check-input" type="checkbox" checked disabled>
                     <label class="form-check-label">
-                        Necessary Cookies (Always Active)
+                        {{__('Necessary Cookies (Always Active)')}}
                     </label>
                 </div>
 
                 <div class="form-check form-switch mb-3">
                     <input class="form-check-input" type="checkbox" id="analyticsCookies">
                     <label class="form-check-label">
-                        Analytics Cookies
+                        {{__('Analytics Cookies')}}
                     </label>
                 </div>
 
                 <div class="form-check form-switch">
                     <input class="form-check-input" type="checkbox" id="marketingCookies">
                     <label class="form-check-label">
-                        Marketing Cookies
+                       {{__('Marketing Cookies')}}
                     </label>
                 </div>
             </div>
             <div class="modal-footer">
-                <button class="btn btn-primary" onclick="savePreferences()">Save Preferences</button>
+                <button class="btn btn-primary" onclick="savePreferences()">{{__('Save Preferences')}}</button>
             </div>
         </div>
     </div>
