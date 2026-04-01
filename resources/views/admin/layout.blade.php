f471ea6b8b3a6ac1e5a8706597c6c9433ef4a4fe
Lakmal Wijerathna <samdynmic@gmail.com>
2026-04-01 08:53:30 +0200

    dsdsd

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -315,7 +315,7 @@
                         <li class="nav-item">
                             <a class="nav-link nav-link d-flex align-items-center @if (Route::currentRouteName() == 'admin.gift-card') active @endif"
                                 href="{{ route('admin.gift-card') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                     fill="currentColor" class="bi bi-gift" viewBox="0 0 16 16">
                                     <path
                                         d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1v7.5a1.5 1.5 0 0 1-1.5 1.5h-11A1.5 1.5 0 0 1 1 14.5V7a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A3 3 0 0 1 3 2.506zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43zM9 3h2.932l.023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0zM1 4v2h6V4zm8 0v2h6V4zm5 3H9v8h4.5a.5.5 0 0 0 .5-.5zm-7 8V7H2v7.5a.5.5 0 0 0 .5.5z" />
@@ -339,7 +339,7 @@
                         <li class="nav-item">
                             <a class="nav-link nav-link d-flex align-items-center @if (Route::currentRouteName() == 'admin.subscribers') active @endif"
                                 href="{{ route('admin.subscribers') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                     fill="currentColor" class="bi bi-rss" viewBox="0 0 16 16">
                                     <path
                                         d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z" />
@@ -363,7 +363,7 @@
                         <li class="nav-item">
                             <a class="nav-link nav-link d-flex align-items-center @if (Route::currentRouteName() == 'admin.pickup') active @endif"
                                 href="{{ route('admin.pickup') }}">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
                                     fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
                                     <path
                                         d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
