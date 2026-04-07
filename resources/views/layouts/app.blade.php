f4a7d1a754a0c635a7b4a32fc9b04858a5a0b380
Lakmal Wijerathna <samdynmic@gmail.com>
2026-04-07 08:08:36 +0200

    titles changed meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     {{-- SEO --}}
-    <meta name="title" content="{{ __('Premium Grass-Fed Tallow Balm for Ultimate Hydration') }}">
+    <meta name="title" content="{{ __('Natural, unique & effective skin care from Croatia!') }}">
     <meta name="description"
-        content="{{ __('Experience the power of 100% natural tallow balm made from grass-fed ingredients. Perfect for dry and sensitive skin, it provides long-lasting moisture, supports skin repair, and keeps your skin soft and healthy.') }}">
+        content="{{ __('Purified beef tallow is one of the ingredients that most closely resembles human skin.') }}">
     <meta name="keywords"
         content="{{ __('tallow balm, tallow skin care, grass fed tallow, natural moisturizer, organic tallow balm, beef tallow skincare, handmade tallow balm, sensitive skin moisturizer, deep hydration balm, natural skin cream, chemical free skincare, traditional skincare, nourishing face balm') }}">
     <meta property="og:image" content="{{ asset('ico/favicon-32x32.png') }}">
