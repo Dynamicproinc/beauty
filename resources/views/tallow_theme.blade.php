362e5126744240ea1ab0ae84a461445542a2bd18
Lakmal Wijerathna <samdynmic@gmail.com>
2026-05-07 10:49:31 +0200

    afdaf

--- resources/views/tallow_theme.blade.php
+++ resources/views/tallow_theme.blade.php
@@ -28,7 +28,7 @@
                                     {{-- <span>{{ __('Less is more, even in skincare.') }}</span> --}}
                                 </div>
 
-                                <div class="tt-text-hero-md mb-3">
+                                <div class="tt-text-hero-md mb-4">
                                    
                                     <p class="hero-text-md">{{ __('Pure care the kind that skin has always understood') }}</p>
                                 </div>
