4c4e2c1fb9ca36e5508210e72774bf07eb7c1023
Lakmal Wijerathna <samdynmic@gmail.com>
2026-05-12 07:35:05 +0200

    dsdsdsd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -53,9 +53,9 @@
 <body>
     <div id="app">
         {{-- under the constructor --}}
-        {{-- <div class="fixed-bottom bg-tallow-theme text-white p-3 text-center">
+        <div class="fixed-bottom bg-tallow-theme text-white p-3 text-center">
             Gradimo nešto sjajno! Narudžbe još nisu otvorene, ali uskoro ćemo biti spremni za vas 😊
-        </div> --}}
+        </div>
         {{--  --}}
         @include('inc.navbar_detailed')
         <main class="">
