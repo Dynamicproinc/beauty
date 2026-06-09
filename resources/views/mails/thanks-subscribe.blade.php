bcd85ccd8de1e1668a24aa93cc5d61ba4727bd73
lakmal wijerathne <samdynmic@gmail.com>
2026-06-09 13:33:05 +0200

    all updated from genesis computer

--- resources/views/mails/thanks-subscribe.blade.php
+++ resources/views/mails/thanks-subscribe.blade.php
@@ -1,98 +1,105 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>{{ __('Thanks for Subscribing') }}</title>
-    <style>
-      body {
-        overflow-x: hidden;
-        color: #3a3a3a;
-        font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-        background-color: #f5f5f5;
-        
-      }
-      p{
-        font-size: 14px;
-      }
-      main {
-        width: 100%;
-        min-height: 100%;
-        display: flex;
-        justify-content: center;
-        padding: 32px;
-        align-items: center;
-      }
-      .main-content {
-        width: 450px;
 
-        height: 100%;
-        padding: 32px;
-        overflow: hidden;
-      }
-      .cover-image {
-        width: 100%;
-        height: auto;
-      }
-      .body-text-content {
-        text-align: center;
-      }
-      .footer-content {
-        margin-top: 32px;
-       height: 100px ;
-        background-color: #000;
-        color: #fff;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        padding: 8px 16px;
-        font-size: 14px;
-        background-color: #A5AE9A;
-       
-      }
-      h4{
-        margin: 0;
-      }
-    </style>
-  </head>
-  <body>
-    <main>
-      <div class="main-content">
-        <div>
-  <img
-    class="cover-image"
-    src="{{ asset('media/images/email_tallow_text.png') }}"
-    alt="{{ __('Tallow Skin Care') }}"
-  />
-</div>
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>{{ __('Thanks for Subscribing') }}</title>
+  <style>
+    body {
+      overflow-x: hidden;
+      color: #3a3a3a;
+      font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+      background-color: #f5f5f5;
 
-<div class="body-text-content">
-  <h3 style="text-transform: uppercase">{{ __('trnid243') }}</h3>
-  <p>
-    {{ __('trnid244') }}
-  </p>
-</div>
+    }
 
-<div class="footer-content">
-  <div style="border-right: 1px solid #fff; padding-right: 16px">
-    <h4>{{ __('Tallow Skin Care') }}</h4>
-    <small>
-      {{ __('Tallow Skin Care j.d.o.o') }}<br />
-      {{ __('Donja Švarča 79c') }}<br />
-      {{ __('47000 Karlovac, Hrvatska.') }}
-    </small>
-  </div>
+    p {
+      font-size: 14px;
+    }
 
-  <div style="width:50%;padding-left: 16px;">
-    <p>
-      <small>
-        {{ __('trnid245') }}
-        <a href="{{route('unsubscribe-email',[$data['reference'], $data['email']])}}">{{ __('trnid246') }}</a>
-      </small>
-    </p>
-  </div>
-</div>
+    main {
+      width: 100%;
+      min-height: 100%;
+      display: flex;
+      justify-content: center;
+      padding: 32px;
+      align-items: center;
+    }
+
+    .main-content {
+      width: 450px;
+
+      height: 100%;
+      padding: 32px;
+      overflow: hidden;
+    }
+
+    .cover-image {
+      width: 100%;
+      height: auto;
+    }
+
+    .body-text-content {
+      text-align: center;
+    }
+
+    .footer-content {
+      margin-top: 32px;
+      height: 100px;
+      background-color: #000;
+      color: #fff;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 8px 16px;
+      font-size: 14px;
+      background-color: #A5AE9A;
+
+    }
+
+    h4 {
+      margin: 0;
+    }
+  </style>
+</head>
+
+<body>
+  <main>
+    <div class="main-content">
+      <div>
+        <img class="cover-image" src="{{ asset('media/images/email_tallow_text.png') }}"
+          alt="{{ __('Tallow Skin Care') }}" />
       </div>
-    </main>
-  </body>
+
+      <div class="body-text-content">
+        <h3 style="text-transform: uppercase">{{ __('trnid243') }}</h3>
+        <p>
+          {{ __('trnid244') }}
+        </p>
+      </div>
+
+      <div class="footer-content">
+        <div style="border-right: 1px solid #fff; padding-right: 16px">
+          <h4>{{ __('Tallow Skin Care') }}</h4>
+          <small>
+            {{ __('Tallow Skin Care j.d.o.o') }}<br />
+            {{ __('Donja Švarča 79c') }}<br />
+            {{ __('47000 Karlovac, Hrvatska.') }}
+          </small>
+        </div>
+
+        <div style="width:50%;padding-left: 16px;">
+          <p>
+            <small>
+              {{ __('trnid245') }}
+              <a href="{{route('unsubscribe-email',[$data['reference'], $data['email']])}}">{{ __('trnid246') }}</a>
+            </small>
+          </p>
+        </div>
+      </div>
+    </div>
+  </main>
+</body>
+
 </html>
