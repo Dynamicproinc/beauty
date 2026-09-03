ce75e94083bcbc13c4501cb323d28688f70e7a7f
Lakmal Wijerathna <samdynmic@gmail.com>
2026-09-03 16:16:46 +0200

    dsdS

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -444,6 +444,9 @@
             </main>
         </div>
     </div>
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js">
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
         integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
