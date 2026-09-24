a5d3a4ab6cc5b4f073dbdc2d3b5d969e8dd15778
Lakmal Wijerathna <samdynmic@gmail.com>
2026-09-24 10:54:57 +0200

    new update to the show product

--- resources/views/shop/showproduct.blade.php
+++ resources/views/shop/showproduct.blade.php
@@ -121,8 +121,8 @@
                             {{--  --}}
                             {{-- @livewire('shop.product.addcart',['product' => $product]) --}}
                             <livewire:shop.product.addcart :product="$product" />
-                            <div style="font-size: 12px !important" class="mb-3">
-                                <small class="small text-muted" style="font-size: 12px !important"> @if($product->last_price_update)<i class="bi bi-clock-history" style="font-size: 12px !important"></i> @endif {{ $product->last_price_update ?? $product->last_price_update  }}</small>
+                            <div style="font-size: 14px !important" class="mb-3">
+                                <small class="small fw-bold text-muted" style="font-size: 14px !important"> @if($product->last_price_update)<i class="bi bi-clock-history" style="font-size: 14px !important"></i> @endif {{ $product->last_price_update ?? $product->last_price_update  }}</small>
                             </div>
                             <div class="small-note-areas mb-3">
                                 <p class="small-note">{{ __('delivery.note') }}</p>
